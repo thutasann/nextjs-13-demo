35f3c3a5e5e00537862e60e60e4c9dacd865224e
thuta_yoma <thutasann@yomabank.com>
2022-11-05 22:52:43 +0630

    implemented `SSG`

--- app/todos/[todoId]/page.tsx
+++ app/todos/[todoId]/page.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Todo } from '../../../typings';
 import Link from 'next/link';
+import { notFound } from 'next/navigation'
+
+export const dynamicParams = true;
 
 type PageProps = {
     params: {
@@ -10,15 +13,23 @@ type PageProps = {
 
 const fetchSingleTodo = async (todoId: string) => {
     const res = await fetch(
-        `https://jsonplaceholder.typicode.com/todos/${todoId}`
+        `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+        {
+            next: {
+                revalidate: 60
+            }
+        }
     );
     const todo = await res.json();
     return todo;
-}
+};
 
 const TodoPage = async ({ params: {todoId} }: PageProps) => {
 
     const todo: Todo  = await fetchSingleTodo(todoId);
+
+    // if not ID -> 404
+    if(!todo.id) return notFound();
 
     return (
         <div className='p-10 m-2 bg-blue-200 shadow-lg'>
@@ -38,4 +49,23 @@ const TodoPage = async ({ params: {todoId} }: PageProps) => {
     )
 }
 
-export default TodoPage
+export default TodoPage;
+
+/**
+ * Static Paths
+ */
+export async function generateStaticParams(){
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos/", 
+    { next: { revalidate: 60 }});
+    const todos : Todo[] = await res.json();
+
+    // For this DEMO, we are only prebuilding the first 10 pages to avoid being rate limited by the DEMO API
+    // const trimmedTodos = todos.splice(0, 10);
+
+    return todos.map((todo) => ({
+        todoId: todo.id.toString()
+    }))
+
+    // [ {todoId: '1'}, {todoId: '2'} ]
+
+}
