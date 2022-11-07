86ecf6cebae87db47924a78f9d1b65e70c1ec6b6
thutasann <thutasann@gmail.com>
2022-11-07 21:03:34 +0630

    `React Suspense`

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import TodoList from './(users)/todos/TodoList'
 
 const Home = () => {
     return (
-        <div className='text-blue-500 font-[700] text-[20px]
+        <div className='text-[20px]
         ml-5 mt-4'>
-            This is NEXT JS 13 DEMO
+            <Suspense fallback={<p>Loading Todos...</p>}>
+                <h1>Loading Todos</h1>
+                <div className='flex space-x-2'>
+                    {/* @ts-ignore */}
+                    <TodoList/>
+                </div>
+            </Suspense>
+            <Suspense fallback={<p>Loading Shopping Trolley...</p>}>
+                <div className='flex space-x-2'>
+                    {/* @ts-ignore */}
+                    <TodoList/>
+                </div>
+                <h1>Loading Shopping Trolley</h1>
+            </Suspense>
         </div>
     )
 }
